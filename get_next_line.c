bc998c8285b790d31c867bb2b89a34e5ce4fb4e4
Oscar Amado de Carvalho <o.amado@orange.fr>
2023-01-30 21:14:47 +0000

    Complete.

---
+++ get_next_line.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozerbib- <ozerbib-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 13:17:15 by ozerbib-          #+#    #+#             */
+/*   Updated: 2023/01/30 16:03:23 by ozerbib-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char		unloader[BUFFER_SIZE + 1];
+	char			*loader;
+	int				i;
+	
+	i = 0;
+	if (BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
+	{
+		while (unloader[i])
+			unloader[i++] = '\0';
+		return (NULL);
+	}
+	loader = NULL;
+	while (unloader[0] || read(fd, unloader, BUFFER_SIZE) > 0)
+	{
+		loader = ft_join(loader, unloader);
+		if (ft_cleaner(unloader) > 0)
+			break ;
+	}
+	return (loader);
+}
